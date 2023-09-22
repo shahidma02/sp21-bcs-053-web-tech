0740c1dd1340baf1129a1824d36db25414685efe
Amena Shahid <amenashahid58@gmail.com>
2023-09-22 22:14:01 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="py-40 pl-20">
+        <h1 class="mb-8 text-6xl font-bold bg-gradient-to-r  from-purple-500 to-pink-500 text-transparent bg-clip-text">
+          Scribbling Brilliance,
+          <br />
+          One Page at a Time
+        </h1>
+        <div className="flex">
+          <p className="pr-10 border-r-2 hover:text-pink-500">Marketing</p>
+          <p className="px-10 border-r-2  hover:text-pink-500">Design</p>
+          <p className="px-10 border-r-2 hover:text-pink-500">E-commerce</p>
+          <p className="px-10   hover:text-pink-500">Fashion</p>
+        </div>
+      </div>
     </div>
   );
 }

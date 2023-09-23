69d93db63758db8766f341c88802e2272eb857db
Amena Shahid <amenashahid58@gmail.com>
2023-09-23 13:02:34 +0500

    Initial2

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ function App() {
         <h1 class="mb-8 text-6xl font-bold bg-gradient-to-r  from-purple-500 to-pink-500 text-transparent bg-clip-text">
           Scribbling Brilliance,
           <br />
-          One Page at a Time
+          One Page at a Time!
         </h1>
         <div className="flex">
           <p className="pr-10 border-r-2 hover:text-pink-500">Marketing</p>
           <p className="px-10 border-r-2  hover:text-pink-500">Design</p>
-          <p className="px-10 border-r-2 hover:text-pink-500">E-commerce</p>
-          <p className="px-10 border-r-2 hover:text-pink-500">E-commerce</p>
           <p className="px-10 border-r-2 hover:text-pink-500">E-commerce</p>
           <p className="px-10   hover:text-pink-500">Fashion</p>
         </div>
